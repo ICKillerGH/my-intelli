a6633e148ff51d7ad14c807b8c4894608a57bd31
Alexis Navarro <alexisnavarro1994@gmail.com>
2024-06-22 09:29:52 -0400

    Add books model

---
+++ database/migrations/2024_06_22_121956_create_books_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Author;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('isbn')->unique();
+            $table->string('front_page');
+            $table->text('synopsis');
+            $table->date('number_of_pages');
+            $table->string('published_at');
+            $table->foreignId('author_id')->constrained('authors')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
--- database/seeders/AuthorSeeder.php
+++ database/seeders/AuthorSeeder.php
@@ -3,16 +3,28 @@
 namespace Database\Seeders;
 
 use App\Models\Author;
+use App\Models\Book;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class AuthorSeeder extends Seeder
 {
+    use WithoutModelEvents;
+    
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        Author::factory(20)->create();
+        $authors = Author::factory(20)->create();
+
+        foreach ($authors as $author) {
+            $booksCount = random_int(1, 5);
+            
+            Book::factory($booksCount)->for($author)->create();
+
+            $author->books_count = $booksCount;
+            $author->save();
+        }
     }
 }
